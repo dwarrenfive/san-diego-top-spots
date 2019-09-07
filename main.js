7ea0699857dcc97cd4ccb97ebf8ea32d0e324d3f
David <dwarrenfive@gmail.com>
2019-09-06 18:03:15 -0700

    added jquery script, pulled data.json file to json function

--- main.js
+++ main.js
@@ -1,4 +1,29 @@
 $(document).ready(function() {
-// write your code here
+  $.getJSON("data.json", function(json) {
+    // console.log(json);
 
+    /* jsonData.getString("name");
+  jsonData.getString("description");
+  jsonData.getString("location"); */
+
+    // var hotSpots = getJSON("data.json");
+
+    var table = document.getElementById("table");
+
+    var tr = table.insertRow(-1);
+
+    for (var i = 0; i < json.length; i++) {
+      tr = table.insertRow(-1);
+      var tabCell = tr.insertCell(-1);
+      tabCell.innerHTML = json[i];
+    }
+
+    /* var divContainer = document.getElementById("table");
+    divContainer.innerHTML = "";
+    divContainer.appendChild(table); */
+  });
 });
+
+/* function handleClickEvent() {
+  return $(document);
+} */
