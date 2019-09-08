62dc7714757ff398c03395a41c5b07629482b3ca
David <dwarrenfive@gmail.com>
2019-09-07 18:06:16 -0700

    added more style, cleaned up js

--- main.js
+++ main.js
@@ -1,13 +1,14 @@
 $(document).ready(function() {
   $.getJSON("data.json", function(json) {
-    console.log(json);
+    // console.log(json);
 
     var data = `<tbody>`;
     for (var i = 0; i < json.length; i++) {
       data += `<tr>`;
       data += `<td> ${json[i].name} <td>`;
       data += `<td> ${json[i].description} <td>`;
-      data += `<td> ${json[i].location} <td>`;
+      data += `<td><a href='https://www.google.com/maps?q=${json[i].location}'>
+      <img src="maps-icon.png" style="width:62px;height:62px;border:0;"><td>`;
       data += `<tr>`;
     }
     data += `<tbody`;
